1d494b97430eca48542ec4d461302763e33d1ae5
Ben Champagne <donkeykong797@gmail.com>
2023-03-06 14:19:19 -0500

    Added Jolt Move

--- Maestro/ModMain.gd
+++ Maestro/ModMain.gd
@@ -1,4 +1,7 @@
 extends Node
 
+### Dev Note
+# - Make sure to append "res://Maestro/characters/Maestro/Maestro.tscn" to "res://Global.gd" `name_paths`
+
 func _init(modLoader = ModLoader):
 	modLoader.installScriptExtension("res://Maestro/CharacterSelect.gd")
