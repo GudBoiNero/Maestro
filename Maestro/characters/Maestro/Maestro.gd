86920bf64378d19c613ca8a7ba05aac4e534b556
Ben Champagne <donkeykong797@gmail.com>
2023-03-08 11:21:37 -0500

    Added Custom Parry and Fixed JoltFollowup

--- Maestro/characters/Maestro/Maestro.gd
+++ Maestro/characters/Maestro/Maestro.gd
@@ -3,8 +3,23 @@ extends Fighter
 onready var shooting_arm = $"%ShootingArm"
 onready var shooting_point = $"%ShootingPoint"
 
+onready var default_parry = preload("res://fx/ParryEffect.tscn")
+
 var shot_dir_x = 100
 var shot_dir_y = 0
 
 func _ready():
 	shooting_arm.set_material(sprite.get_material())
+
+
+func spawn_particle_effect(particle_effect:PackedScene, pos:Vector2, dir = Vector2.RIGHT):
+	if ReplayManager.resimulating:
+		return
+	
+	# Custom Parry
+	if particle_effect == default_parry:
+		particle_effect = preload("res://Maestro/characters/Maestro/particles/ParryEffect.tscn")
+	
+	if not initialized:
+		yield (self, "initialized")
+	call_deferred("_spawn_particle_effect", particle_effect, pos, dir)
